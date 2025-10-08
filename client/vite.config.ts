432c93a18f6ef60e3e16b2270dca8f20ab8b2a4c
shravani <shravanibeni07@gmail.com>
2025-10-09 02:48:46 +0530

    increased-size

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -40,6 +40,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // 5 MB limit
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/tile.openstreetmap.org\/.*$/,
